0e04df1e749f4b50b15b2b7c358c37a3b3d49abb
edwinchen85 <edwinchen85@gmail.com>
2017-03-03 01:21:58 +0800

    Assigning click events to the tabs. Animating the panels based on the tab clicked.

--- js/sliding_panels.js
+++ js/sliding_panels.js
@@ -6,11 +6,29 @@ $(document).ready(function() {
 
   window.panelWidth = $('.sp').width();
 
-  $('panel_container .panel').each(function(index) {
+  $('.panel_container .panel').each(function(index) {
 
     $(this).css({'width':window.panelWidth+'px','left':(index*window.panelWidth)+'px'});
 
     $('.sp .panels').css('width',(index+1)*window.panelWidth+'px');
 
   });
+
+  $('.sp .tabs span').each(function() {
+
+    $(this).on('click', function() {
+      changePanels( $(this).index() );
+    });
+
+  })
 });
+
+function changePanels(newIndex) {
+
+  var newPanelPosition = (window.panelWidth*newIndex) * -1;
+  var newPanelHeight = $('.sp .panel:nth-child('+(newIndex+1)+')').find('.panel_content').height() + 15;
+
+  $('.sp .panels').animate({left:newPanelPosition},1000);
+  $('.sp .panel_container').animate({height:newPanelHeight},1000);
+
+}
